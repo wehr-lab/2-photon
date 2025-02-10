40adc8a0a35f741a1b89e13f09ee1b3ea1379172
“Praves-Lamichhane” <praveslamichhane@gmail.com>
2025-02-09 21:07:10 -0800

    2p_fc_individual_sessions normalizes degree value and saves FC matrix and reloads the saved matrix instead of loading raw data from server

--- src/2p_fc_individual_sessions.py
+++ src/2p_fc_individual_sessions.py
@@ -11,41 +11,50 @@ sessionPaths = ["/Volumes/projects/2P5XFAD/JarascopeData/wehr3204/01-17-25-002/s
 
 # sessionPaths = ["/Volumes/projects/2P5XFAD/JarascopeData/wehr3204/01-17-25-002/suite2p/plane0"]
 
-dFFs = {} ## this will store the dFF values for each session
+# dFFs = {} ## this will store the dFF values for each session
 
-for session in sessionPaths:
-    ## load data 
-    F, Fneu, spks, stat, ops, iscell = ephys_analysis.load2P(session) 
+# for session in sessionPaths:
+#     ## load data 
+#     F, Fneu, spks, stat, ops, iscell = ephys_analysis.load2P(session) 
 
-    print("Data loaded for session: ", session)
+#     print("Data loaded for session: ", session)
 
-    ## NOTE: data loading is taking a long time. This is because the data is being loaded from a network drive. This will be the same when we work on talapas. Maybe there is a better way? 
+#     ## NOTE: data loading is taking a long time. This is because the data is being loaded from a network drive. This will be the same when we work on talapas. Maybe there is a better way? 
 
-    indicesCells = ephys_analysis.getIndices(iscell, prob=0.5)
-    fCells = ephys_analysis.extractCells(F, indicesCells)
-    fneuCells = ephys_analysis.extractCells(Fneu, indicesCells)
+#     indicesCells = ephys_analysis.getIndices(iscell, prob=0.5)
+#     fCells = ephys_analysis.extractCells(F, indicesCells)
+#     fneuCells = ephys_analysis.extractCells(Fneu, indicesCells)
 
-    ## neuropil correction
-    fCorrected = ephys_analysis.correct_neuropil(fCells, fneuCells, npil_coeff=0.7)
+#     ## neuropil correction
+#     fCorrected = ephys_analysis.correct_neuropil(fCells, fneuCells, npil_coeff=0.7)
 
-    ## remove negative values from the corrected fluorescence values
-    fCorrectedShifted = ephys_analysis.absFluoresce(fCorrected)
+#     ## remove negative values from the corrected fluorescence values
+#     fCorrectedShifted = ephys_analysis.absFluoresce(fCorrected)
 
-    ## another plot to visualize the time series of the corrected fluorescence values and to select a threshold
-    # plt.plot(fCorrectedShifted[0, :])
+#     ## another plot to visualize the time series of the corrected fluorescence values and to select a threshold
+#     # plt.plot(fCorrectedShifted[0, :])
 
-    ## draw a horizontal line at the 10th percentile
-    # plt.axhline(np.percentile(fCorrectedShifted[0, :], 20), color="red")
-    # plt.show() 
+#     ## draw a horizontal line at the 10th percentile
+#     # plt.axhline(np.percentile(fCorrectedShifted[0, :], 20), color="red")
+#     # plt.show() 
 
-    ## compute dF/F using a sliding window
-    dFF = ephys_analysis.computeDFFSlide(fCorrectedShifted, window=200, percentile=20) ## note that the window size and the percentile are parameters are totally arbitrary for now 
-    print("Sliding window dF/F computed for session: ", session)
+#     ## compute dF/F using a sliding window
+#     dFF = ephys_analysis.computeDFFSlide(fCorrectedShifted, window=200, percentile=20) ## note that the window size and the percentile are parameters are totally arbitrary for now 
+#     print("Sliding window dF/F computed for session: ", session)
 
-    # ## save data for further processing 
-    # np.save(os.path.join(DATA_PATH_SAVE, "dF_F_100824_sliding_f_nought.npy"), dFF)
-    dFFConvolve = ephys_analysis.convolve_spikes(dFF, sigma=10)
-    dFFs[session] = dFFConvolve
+#     # ## save data for further processing 
+#     # np.save(os.path.join(DATA_PATH_SAVE, "dF_F_100824_sliding_f_nought.npy"), dFF)
+#     dFFConvolve = ephys_analysis.convolve_spikes(dFF, sigma=10)
+#     dFFs[session] = dFFConvolve
+
+## save the dFF values for each session as .npy files and load them -> faster
+savePath = sessionPaths[0].split("/")[:-3]
+savePath = "/".join(savePath) + "/"
+# np.savez(savePath + "dFFs.npy", dFFs)
+
+# ## load the dFF values for each session
+savedFile = np.load(savePath + "dFFs.npy.npz", allow_pickle=True)
+dFFs = savedFile["arr_0"].item()
 
 fcMatrix = {}
 fcMatrixBin = {}
@@ -63,6 +72,16 @@ for session, dFF in dFFs.items():
     ## remove autocorrelations
     np.fill_diagonal(fcMatrixBin[session], 0)
 
+## cluster the non binarized matrix also
+for i, (session, fc) in enumerate(fcMatrix.items()):
+    g = sns.clustermap(fc, method="ward", cmap="coolwarm", square=True,  xticklabels=False, yticklabels=False, cbar=True, dendrogram_ratio=(0.1, 0.1), col_cluster=True, row_cluster=True)
+    # if g.cax is not None: ## decided to include the colorbar
+    #     g.cax.set_visible(False)
+    g.ax_row_dendrogram.set_visible(False)
+    g.ax_col_dendrogram.set_visible(False)
+    g.ax_heatmap.set_title(f"Clustering of unbinarized correlation matrix for {session.split('/')[-3]}, Cells={fc.shape[0]}", y=1.05, fontsize=15, fontweight='bold')
+    plt.show()
+
 ## plot the functional connectivity matrices
 # fig, axs = plt.subplots(1, 3, figsize=(15, 5))
 for i, (session, fc) in enumerate(fcMatrixBin.items()):
@@ -71,7 +90,7 @@ for i, (session, fc) in enumerate(fcMatrixBin.items()):
     #     g.cax.set_visible(False)
     g.ax_row_dendrogram.set_visible(False)
     g.ax_col_dendrogram.set_visible(False)
-    g.ax_heatmap.set_title(f"Clustering of pairwise correlation matrix for {session.split('/')[-3]}, Cells={fc.shape[0]}", y=1.05, fontsize=15, fontweight='bold')
+    g.ax_heatmap.set_title(f"Clustering of binarized correlation matrix for {session.split('/')[-3]}, Cells={fc.shape[0]}", y=1.05, fontsize=15, fontweight='bold')
     plt.show()
 
 
@@ -102,13 +121,17 @@ for i, graph in enumerate(graphs):
 
 ## plot all the graph metrics
 degrees = [np.nan_to_num(np.array(graphMatrics[i]["degree"])) for i in range(len(graphMatrics))]
+
+# ## normalize the degree values according to the number of nodes in the graph
+degreesNorm = [degree/(graphs[i].vcount()) for i, degree in enumerate(degrees)] ## think this shoud work 
+
 clusterings = [np.nan_to_num(np.array(graphMatrics[i]["clustering"])) for i in range(len(graphMatrics))]
 pathLengths = [np.nan_to_num(np.array(graphMatrics[i]["path_length"])) for i in range(len(graphMatrics))]
 
 fig, axs = plt.subplots(1, 3, figsize=(15, 5))
-axs[0].boxplot(degrees)
+axs[0].boxplot(degreesNorm)
 axs[0].set_xticklabels(["Session 1", "Session 2", "Session 3"]) 
-axs[0].set_title("Degree")
+axs[0].set_title("Degree Normalized")
 axs[1].boxplot(clusterings)
 axs[1].set_xticklabels(["Session 1", "Session 2", "Session 3"]) 
 axs[1].set_title("Clustering")
