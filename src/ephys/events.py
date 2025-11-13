574adce7dacb4630c6d65da7ebb3d97762a38ed2
Praves Lamichhane <praveslamichhane@gmail.com>
2025-11-13 15:13:02 -0800

    adds tuning curve and population responses to tones

--- src/ephys/events.py
+++ src/ephys/events.py
@@ -1,16 +1,17 @@
-from utils.util_funcs import extract_value
+from utils.util_funcs import extract_value, strcmp
+
 
 class Events:
-    '''
-    Initialize the Events class with fields as attributes. 
-    Args: one event from the eventsFile. Data type should be a numpy record array with named fields. 
-    Output: a class object with the fieldnames as attributes 
-    '''    
-    def __init__(self, event):
+    """
+    Initialize the Events class with fields as attributes.
+    Args: one event from the eventsFile. Data type should be a numpy record array with named fields.
+    Output: a class object with the fieldnames as attributes
+    """
 
-        if not hasattr(event, 'dtype') or not event.dtype.names:
-            raise ValueError('Input should be a numpy record array with named fields.')
-        
+    def __init__(self, event):
+        if not hasattr(event, "dtype") or not event.dtype.names:
+            raise ValueError("Input should be a numpy record array with named fields.")
+
         self.fields = event.dtype.names
 
         ## set attributes for each field in the event
@@ -20,4 +21,4 @@ class Events:
             setattr(self, field, value)
 
     def __repr__(self):
-        return f"Events({', '.join([f'{field}={getattr(self, field)}' for field in self.fields])})" 
+        return f"Events({', '.join([f'{field}={getattr(self, field)}' for field in self.fields])})"
